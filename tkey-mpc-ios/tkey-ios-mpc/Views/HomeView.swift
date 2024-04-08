02cff4d62e82f9528b9cd862fc0e949f43f7de45
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2024-04-08 22:56:42 +0530

    improve example

--- tkey-mpc-ios/tkey-ios-mpc/Views/HomeView.swift
+++ tkey-mpc-ios/tkey-ios-mpc/Views/HomeView.swift
@@ -16,124 +16,153 @@ struct HomeView: View {
     @State private var question: String = ""
     
     var body: some View {
-        ZStack{
-            List {
-                
-                Section(
-                    header: Text("Chain interactions"),
-                    content: {
-                        Button(
-                            action: {
-                                thresholdKeyViewModel.signMessage{
-                                    signedMessage,error in
-                                    self.signedMessage = signedMessage
-                                }
-                            },
-                            label: {
-                                Text("Sign Message")
-                            }
-                        )
-                        
-                        if(signedMessage != nil) {
-                            Text(signedMessage!)
+        let address = thresholdKeyViewModel.address
+        NavigationView{
+            ZStack {
+                Form {
+                    Section(
+                        header: Text("Public Address"),
+                        content: {
+                            Button(
+                                action: {
+                                    UIPasteboard.general.string = address
+                                    thresholdKeyViewModel.showAlert = true
+                                    thresholdKeyViewModel.alertContent = "Address copied to clipbaord!"
+                                }, label: {
+                                    Text(address!)
+                                })
                         }
-                        
-                        Button(
-                            action: {
-                                thresholdKeyViewModel.sendTransaction {
-                                    hash, error  in
-                                    self.hash = hash
+                    )
+                    Section(
+                        header: Text("Chain interactions"),
+                        content: {
+                            Button(
+                                action: {
+                                    thresholdKeyViewModel.signMessage{
+                                        signedMessage,error in
+                                        self.signedMessage = signedMessage
+                                    }
+                                },
+                                label: {
+                                    Text("Sign Message")
                                 }
-                            },
-                            label: {
-                                Text("Send 0.001 ETH")
+                            )
+                            
+                            if(signedMessage != nil) {
+                                Text(signedMessage!)
                             }
-                        )
-                        
-                        if(hash != nil) {
-                            Text(hash!)
+                            
+                            
+                            Button(
+                                action: {
+                                    thresholdKeyViewModel.sendTransaction {
+                                        hash, error  in
+                                        self.hash = hash
+                                    }
+                                },
+                                label: {
+                                    Text("Send 0.001 ETH")
+                                }
+                            )
+                            
+                            
+                            if(hash != nil) {
+                                Link(
+                                    hash!,
+                                    destination: URL(
+                                        string: "https://sepolia.etherscan.io/tx/\(hash!)"
+                                    )!
+                                ).underline()
+                            }
+                            
+                            Text("The sample uses Eth Sepolia, you can choose any EVM network of your choice. Send 0.001 ETH will perform self transfer of ETH. You'll need to have Sepolia faucet to perform transaction.").font(.caption)
                         }
-                    }
-                )
-                
-                Section(
-                    header: Text("Other TSS Factors"),
-                    content: {
-                        let areOtherFactorsPresent = thresholdKeyViewModel.factorPubs.count > 0
-                        if(areOtherFactorsPresent) {
-                            ForEach(Array(thresholdKeyViewModel.factorPubs), id: \.self) { factorPub in
-                                HStack(
-                                    alignment: .top,
-                                    spacing: 24,
-                                    content: {
-                                        Text(factorPub)
-                                        Button(action: {
-                                            withAnimation {
-                                                thresholdKeyViewModel.deleteFactor(
-                                                    deleteFactorPub: factorPub
-                                                )
+                    )
+                    
+                    Section(
+                        header: Text("TSS Factors PubKey"),
+                        content: {
+                            let areOtherFactorsPresent = thresholdKeyViewModel.factorPubs.count > 0
+                            if(areOtherFactorsPresent) {
+                                ForEach(Array(thresholdKeyViewModel.factorPubs), id: \.self) { factorPub in
+                                    HStack(
+                                        alignment: .top,
+                                        spacing: 24,
+                                        content: {
+                                            Text(factorPub)
+                                            Button(action: {
+                                                withAnimation {
+                                                    thresholdKeyViewModel.deleteFactor(
+                                                        deleteFactorPub: factorPub
+                                                    )
+                                                }
+                                            }) {
+                                                Label("",systemImage: "trash")
                                             }
-                                        }) {
-                                            Label("",systemImage: "trash")
-                                        }
-                                    })
+                                        })
+                                }
+                            } else {
+                                Text("Only device factor found. Please create other TSS factor.")
                             }
-                        } else {
-                            Text("Only device factor found. Please create other TSS factor.")
                         }
-                    }
-                )
+                    )
+                    
+                    Section(
+                        header: Text("Tss Module"),
+                        content: {
+                            Button(
+                                action: {
+                                    thresholdKeyViewModel.createNewTSSFactor()
+                                },
+                                label: {
+                                    Text("Create new TSS Factor")
+                                }
+                            )
+                        }
+                    )
+                    
+                    Section(
+                        header: Text("Security Question Module"),
+                        content: {
+                            
+                            TextField(
+                                "Enter your security question",
+                                text: $question
+                            )
+                            
+                            
+                            TextField(
+                                "Enter your security Answer",
+                                text: $answer
+                            )
+                            
+                            Button(
+                                action: {
+                                    thresholdKeyViewModel.addSecurityQuestion(
+                                        question: question,
+                                        answer: answer
+                                    )
+                                },
+                                label: {
+                                    Text("Add security question")
+                                }
+                            )
+                        }
+                    )
+                }.blur(radius: thresholdKeyViewModel.isLoaderVisible ? 15 : 0)
                 
-                Section(
-                    header: Text("TSS Operations"),
-                    content: {
-                        Button(
-                            action: {
-                                thresholdKeyViewModel.createNewTSSFactor()
-                            },
-                            label: {
-                                Text("Create new TSS Factor")
-                            }
-                        )
-                       
-                        Text("Security Question")
-                        
-                        TextField(
-                            "Your security question",
-                            text: $question
-                        )
-                        
-                        Text("Security Answer")
-                        
-                        TextField(
-                            "Your security Answer",
-                            text: $answer
-                        )
-                        
-                        Button(
-                            action: {
-                                thresholdKeyViewModel.addSecurityQuestion(
-                                    question: question,
-                                    answer: answer
-                                )
-                            },
-                            label: {
-                                Text("Add security question")
-                            }
-                        )
+                if(thresholdKeyViewModel.isLoaderVisible) {
+                    HStack(
+                        spacing: 8
+                    ) {
+                        ProgressView()
+                        Text("Processing...")
                     }
-                )
-            }.blur(radius: thresholdKeyViewModel.isLoaderVisible ? 15 : 0)
-            
-            if(thresholdKeyViewModel.isLoaderVisible) {
-                HStack {
-                    ProgressView()
-                    Text("Processing...")
                 }
             }
         }.alert(isPresented: $thresholdKeyViewModel.showAlert, content: {
             Alert(title: Text(thresholdKeyViewModel.alertContent))
-
+            
         })
     }
 }
